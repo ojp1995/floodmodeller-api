3cf60beb2bdbcef4ea94da90b319cbf96d895e1c
ojp1995 <o.j.phillips@pgr.reading.ac.uk>
2023-02-14 11:48:33 +0000

    adds pseudocode of what information we need

--- toolbox/Model Conversion/reading_in TUFLOW.py
+++ toolbox/Model Conversion/reading_in TUFLOW.py
@@ -1,4 +1,5 @@
 # in this file we will be attempting to reading in a TUFLOW .tcf file and convert to a xml2d file
+# Looking at the Bootle flood case
 
 import sys, mmap, glob, os
 
@@ -6,8 +7,12 @@ sys.path.append(r"C:\Users\phillio\Github\Open_source\floodmodeller-api")
 
 from floodmodeller_api import XML2D
 
+# open blank 2d xml file
+xml = XML2D(r"C:\Users\phillio\Github\xml_2d_test_data\Bootle\2d_simulation_blank.xml")
+
 #importing the TUFLOW data as a text file
 # TUFLOW_data = open(r"C:\Users\phillio\OneDrive - Jacobs\Documents\TUFLOW_examples\Bootle\Bootle\TUFLOW\Runs\BOOT_~e1~_~s1~_~s2~_~s3~_023.tcf")
+#TODO: Replace with try except 
 with open(
 r"C:\Users\phillio\OneDrive - Jacobs\Documents\TUFLOW_examples\Bootle\Bootle\TUFLOW\Runs\BOOT_~e1~_~s1~_~s2~_~s3~_023.tcf",
     'rb', 0) as file, \
@@ -22,7 +27,20 @@ if test_tgc_present == 'true':
     for file in glob.glob("*.tgc"):
         tgc_file_name = file
         print(tgc_file_name)
+        # tgc_filepath = sys.path(r"C:\Users\phillio\OneDrive - Jacobs\Documents\TUFLOW_examples\Bootle\Bootle\TUFLOW\Model\tgc_file_name") S
+        tgc_file_path = os.path.join(r"C:\Users\phillio\OneDrive - Jacobs\Documents\TUFLOW_examples\Bootle\Bootle\TUFLOW\Model", file)
+        tgc_file = open(os.path.join(r"C:\Users\phillio\OneDrive - Jacobs\Documents\TUFLOW_examples\Bootle\Bootle\TUFLOW\Model", file))
+
 else:
     raise Exception('.tgc file not found')
 
+# TODO:
+# Now we want to interogate the tgc file to pick out information
+# Info we want: [xll, yll, dx, nrows, ncols, active_area, rotation]
+# x11, yll - > lower left coordinates of box
+# dx - > spacial component
+# nrows, ncols- > number of rows and columns
+# active_area - > as a .shp file, should be able to pull this out relatively easily
+# rotation -> challenge, rotation between north south and the line we are using. 
+target_variables = ['Grid Size (X,Y)', 'Cell Size']
 
