c9549ba76538fd21d5eaa8013bc6753dd206d426
ojp1995 <o.j.phillips@pgr.reading.ac.uk>
2023-03-13 17:27:51 +0000

    removes old code and adds comments

--- toolbox/Model Conversion/TUFLOW_conversion_test_example.py
+++ toolbox/Model Conversion/TUFLOW_conversion_test_example.py
@@ -10,29 +10,26 @@ import math
 import copy
 from TUFLOW_conversion_functions import *
 
+sys.path.append(r"C:\Users\phillio\Github\Open_source\floodmodeller-api")  # path needed to access FM API
+
 # import floodmodeller_api  # imports the full package
 from floodmodeller_api import DAT, ZZN, IEF, IED, XML2D  # imports individual classes (recommended)
 
-
-
-sys.path.append(r"C:\Users\phillio\Github\Open_source\floodmodeller-api")
-
-#load in the test TUFLOW data
-
+#load in the test TUFLOW data, tgc file and tmf file
 tgc_filepath = pathlib.Path('C:/Users/phillio/OneDrive - Jacobs/Documents/TUFLOW_examples/Bootle/Bootle/TUFLOW/Model/BOOT_023.tgc')
 tgc_folder_path = tgc_filepath.parent
 
 tmf_filepath = pathlib.Path(r"C:\Users\phillio\OneDrive - Jacobs\Documents\TUFLOW_examples\Bootle\Bootle\TUFLOW\Model\BOOT_010.tmf")
-tmf_folder_path = tgc_filepath.parent
+
 # give direction to FM folder where new data will be stored.
 FM_folder_path = pathlib.Path(r"C:\Users\phillio\OneDrive - Jacobs\Documents\TUFLOW_examples\Bootle_FM\Bootle_2D_data")
 FM_folder_name = FM_folder_path.name
-domain_name = "Domain 1"
 
-# loading empty(ish) xml2d file
-# empty_xml = r"C:\Users\phillio\OneDrive - Jacobs\Documents\TUFLOW_examples\Bootle_FM\xml2d_bootle_test.xml"
-xml2d = XML2D()
+domain_name = "Domain 1" # specific domain name.
 
+xml2d = XML2D()  # this loads a blank xml file which will pass validation.
+
+# defining an empty dictionary for the computational area.
 xml2d.domains[domain_name]["computational_area"] = {
     'xll': ...,
     'yll': ...,
@@ -43,13 +40,9 @@ xml2d.domains[domain_name]["computational_area"] = {
     'rotation': ...,
 }
 
-# Think this can be removed as it is being initiated inside the function, need to check.
-# xml2d.domains[domain_name]["roughness"] = {
-#     'type': ...,
-#     'law': ...,
-#     'value': ...,
-# }
+# now we begin step by step converting the data from the tgc file to the xml file.
 TUFLOW_data = convert_tgc_to_list(tgc_filepath)
+
 xll, yll, dx, nrows, ncols, active_area_path_FM, rotation = find_active_area_from_tgc_file(TUFLOW_data, tgc_filepath, FM_folder_path)
 
 xml2d = load_active_area_to_xml( xml2d, xll, yll, dx, nrows, ncols, active_area_path_FM, rotation, FM_folder_name, domain_name)
@@ -61,19 +54,17 @@ df_roughness_ID_val = find_mannings_val_from_tmf(tmf_filepath)
 xml2d = find_and_copy_roughness_to_FM_repo(xml2d, TUFLOW_data, tgc_folder_path, FM_folder_path, domain_name, df_roughness_ID_val)
 
 xml2d = load_roughness_to_xml(xml2d, TUFLOW_data, FM_folder_path, domain_name)
-# change the time given
+
+# change the end time of the simulation
 xml2d.domains["Domain 1"]["time"]["total"] = 1.00
 
-xml2d._write()
+xml2d._write()  # in saving we also use check it passes validation
 
 save_name = 'Bootle_test_API_only.xml'
 xml2d.save(pathlib.Path(FM_folder_path.parent, save_name))
 
-
-
-
-# run
+# run the simualtion with the updated 
 xml2d.simulate()
-# re-run starting with blank xml file
+
 
 
